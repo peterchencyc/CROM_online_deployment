fbae81eb8fa264b8a293f79f208695d5c614d322
peterchencyc <cyc@cs.columbia.edu>
2022-07-21 19:19:50 +0000

    fix optimal sampling minor bug

--- optimal_sampling/execute_diffusion.py
+++ optimal_sampling/execute_diffusion.py
@@ -14,7 +14,7 @@ elif device == 'cuda':
 else:
     exit('invalid device')
 
-run_format = 'python optimal_sampling/run.py -exp diffusion -md weights/Diffusion/case3_dec{device_pt}.pt -me weights/Diffusion/case3_enc{device_pt}.pt -config data/diffusion/config.h5 -ini_cond data/Diffusion/h5_f_0000000000.h5 -data_folder optimal_sampling/training_data/Diffusion -proj_type nonlinear -diff_threshold 1e-15 -step_size_threshold 1e-15 -device {device}'
+run_format = 'python optimal_sampling/run.py -exp diffusion -md weights/Diffusion/case3_dec{device_pt}.pt -me weights/Diffusion/case3_enc{device_pt}.pt -config data/Diffusion/config.h5 -ini_cond data/Diffusion/h5_f_0000000000.h5 -data_folder optimal_sampling/training_data/Diffusion -proj_type nonlinear -diff_threshold 1e-15 -step_size_threshold 1e-15 -device {device}'
 
 command = run_format.format(device = device, device_pt=device_pt)
 
